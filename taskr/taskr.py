4363a477ef2365908fef312d6d5de2aee02dfa6a
Mark Galloway <mgallowa@ualberta.ca>
2015-02-11 18:17:58 -0700

    Taskr v2.0

--- taskr/taskr.py
+++ taskr/taskr.py
@@ -56,7 +56,7 @@ def initdb_command():
 @app.route('/')
 def show_entries():
     db = get_db()
-    cur = db.execute('select category, priority, description from tasks order by priority')
+    cur = db.execute('select category, priority, description, id from tasks order by priority')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
@@ -69,14 +69,37 @@ def add_entry():
     flash('New Task was successfully entered')
     return redirect(url_for('show_entries'))
 
+
 @app.route('/delete', methods=['POST'])
 def delete_entry():
     db = get_db()
-    db.execute('delete from tasks (category, priority, description) values (?, ?, ?)', [request.form['category'], request.form['priority'], request.form['description']])
+    db.execute("delete from tasks where id=(?)", [request.form['Id']])
     db.commit()
-    flash('New entry was successfully posted')
+    flash('Task was successfully deleted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
     return redirect(url_for('show_entries'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
